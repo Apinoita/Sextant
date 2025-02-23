744178bc87aee8e0c955c7dae33c5e49477b9d97
Apinoita <lauri.s.lehtimaki@gmail.com>
2025-02-23 10:16:24 +0200

    Initial commit

---
+++ src/main/java/net/apinoita/sextant/SextantClient.java
@@ -0,0 +1,11 @@
+package net.apinoita.sextant;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SextantClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
